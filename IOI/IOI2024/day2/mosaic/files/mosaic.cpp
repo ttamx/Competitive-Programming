6407e097d74bf2381d94d19754baeecb4339f9d2
ttamx <teetat.tata@gmail.com>
2024-09-16 02:15:27 +0700

    16-09-2024

--- IOI/IOI2024/day2/mosaic/files/mosaic.cpp
+++ IOI/IOI2024/day2/mosaic/files/mosaic.cpp
@@ -4,68 +4,15 @@ using namespace std;
 
 using ll = long long;
 
-const int N=400005;
-const int OFFSET=100000;
-
-struct Fenwick{
-    ll t[N];
-    void update(int i,ll v){
-        for(i+=OFFSET;i<N;i+=i&-i){
-            t[i]+=v;
-        }
-    }
-    ll query(int i){
-        ll res=0;
-        for(i+=OFFSET;i>0;i-=i&-i){
-            res+=t[i];
-        }
-        return res;
-    }
-}f;
-
 vector<ll> mosaic(vector<int> X,vector<int> Y,vector<int> T,vector<int> B,vector<int> L,vector<int> R){
     int n=X.size();
     int q=T.size();
-    vector<ll> ans(q);
-    if(n<=5000){
-        vector<vector<int>> a(n,vector<int>(n));
-        a[0]=X;
-        for(int i=1;i<n;i++)a[i][0]=Y[i];
-        for(int i=1;i<n;i++){
-            for(int j=1;j<n;j++){
-                if(a[i-1][j]+a[i][j-1]==0){
-                    a[i][j]=1;
-                }
-            }
-        }
-        auto b=a;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i>0){
-                    b[i][j]+=b[i-1][j];
-                }
-                if(j>0){
-                    b[i][j]+=b[i][j-1];
-                }
-                if(i>0&&j>0){
-                    b[i][j]-=b[i-1][j-1];
-                }
-            }
-        }
-        for(int i=0;i<q;i++){
-            ans[i]=b[B[i]][R[i]];
-            if(T[i]>0){
-                ans[i]-=b[T[i]-1][R[i]];
-            }
-            if(L[i]>0){
-                ans[i]-=b[B[i]][L[i]-1];
-            }
-            if(T[i]>0&&L[i]>0){
-                ans[i]+=b[T[i]-1][L[i]-1];
-            }
-        }
-        return ans;
+    if(n<3){
+        X.resize(3);
+        Y.resize(3);
+        n=3;
     }
+    vector<ll> ans(q);
     vector<int> a(n),b(n),c(n),d(n);
     a[0]=Y[1];
     b[0]=X[1];
